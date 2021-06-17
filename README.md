0b8756b5ffdea79a871ce76d090640e6c2937930
Sergei Egorov <bsideup@gmail.com>
2021-06-17 22:06:57 +0200

    I miss you, Bintray

---
+++ README.md
@@ -0,0 +1,68 @@
+# TinSalver
+
+This is a tiny script to PGP sign Maven artifacts uploaded to Sonatype Nexus.  
+It is especially helpful for doing releases to Maven Central.
+
+## Prerequisites
+
+Make sure that you have an account on https://keybase.io and that you imported your PGP key.
+
+Also, you will need to install and configure Keybase's CLI.
+
+To run the script itself, you will need NodeJS installed on your machine.
+
+Also, make sure that the dependencies are installed:
+
+```shell
+$ npm install
+```
+
+## Usage
+
+```shell
+$ node tinsalver.mjs sign -u userOrTokenId -p passwordOrToken comexample-java-1234
+```
+
+## How it works
+
+The script will use Nexus' API to find all artifacts in a given staging repository,
+download them and call `keybase pgp sign` to generate signatures.  
+Then, it will upload generated signatures to the same staging repo (hence the user/password requirement),
+so that it can be closed & released.
+
+## Why does it work?
+
+Staged repositories allow uploading multiple files, meaning that the signatures can be uploaded after the CI run.
+
+This eliminates the need to run signing during the CI, and you can sign the result (with your **private** key)
+after CI has finished uploading the artifacts.
+
+## But... does it _really_ work?
+
+Yes! Here is a list of projects that were released with this tool:
+
+- http://github.com/docker-java/docker-java
+- https://github.com/bsideup/jabel
+
+You can check it yourself:
+
+```shell
+$ curl https://keybase.io/bsideup/pgp_keys.asc | gpg --import
+gpg: key 100306F2B3793BF3: public key "keybase.io/bsideup <bsideup@keybase.io>" imported
+gpg: Total number processed: 1
+gpg:               imported: 1
+$ 
+$ wget -q https://repo1.maven.org/maven2/com/github/docker-java/docker-java-api/3.2.9/docker-java-api-3.2.9.pom{,.asc}
+$ 
+$ gpg --verify docker-java-api-3.2.9.pom.asc docker-java-api-3.2.9.pom
+gpg: Signature made Do 17 Jun 17:22:15 2021 CEST
+gpg:                using RSA key BF7CC7DEFE9389D7
+gpg: Good signature from "keybase.io/bsideup <bsideup@keybase.io>" [unknown]
+```
+
+## But... Why?
+
+Bintray was an amazing way to release to Maven Central, but it is no longer available.
+
+As I didn't want to put my GPG private key on CI (yes, even as a secret. Yes, I know about the GHA Environments :D),
+I started thinking "is it possible to do what Bintray did for us, but... manually?". You're reading the result's README.
---
+++ tinsalver.mjs
@@ -0,0 +1,86 @@
+import { parseStringPromise } from 'xml2js';
+import fetch from 'node-fetch';
+import { exec } from 'child_process';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+async function* crawl(url) {
+    let resp = await fetch(url);
+    const xml = await parseStringPromise(await resp.text());
+
+    for (let item of xml?.content?.data[0]['content-item']) {
+        if (item['leaf'][0] == 'true') {
+            yield item;
+        } else {
+            yield* crawl(item['resourceURI'][0]);
+        }
+    }
+}
+
+await yargs(hideBin(process.argv))
+    .option('user', {
+        alias: 'u',
+        type: 'string',
+        required: true,
+        description: 'Sonatype user or token id',
+    })
+    .option('password', {
+        alias: 'p',
+        type: 'string',
+        required: true,
+        description: 'Sonatype password or token id',
+    })
+    .command({
+        command: 'sign <stagedRepoId>',
+        handler: async ({ stagedRepoId, user, password }) => {
+            const stagingURL = `https://oss.sonatype.org/service/local/repositories/${stagedRepoId}/content/`;
+
+            for await (let {
+                resourceURI: [url],
+                text: [text],
+            } of crawl(stagingURL)) {
+                if (text.startsWith('maven-metadata.xml')) {
+                    continue;
+                }
+
+                const ext = text.split('.').slice(-1)[0];
+                if (['asc', 'sha1', 'sha256', 'sha512'].includes(ext)) {
+                    continue;
+                }
+                console.log(url);
+
+                const fileBody = await (await fetch(url)).buffer();
+
+                const signature = await new Promise((resolve, reject) => {
+                    try {
+                        const proc = exec(`keybase pgp sign --detached -i -`, (err, stdout, stderr) => {
+                            console.log(stderr);
+                            if (err) {
+                                reject(err);
+                            } else {
+                                resolve(stdout);
+                            }
+                        });
+                        proc.stdin.write(fileBody);
+                        proc.stdin.end();
+                    } catch (err) {
+                        reject(err);
+                    }
+                });
+
+                console.log(signature);
+
+                const resp = await fetch(`${url}.asc`, {
+                    method: 'PUT',
+                    body: signature,
+                    headers: {
+                        Authorization: 'Basic ' + btoa(`${user}:${password}`),
+                    },
+                });
+                if (resp.status != 201) {
+                    throw `${resp.statusText} ${resp.status}`;
+                }
+                console.log(await resp.text());
+            }
+        },
+    }).argv;
